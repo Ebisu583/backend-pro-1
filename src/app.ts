0adcbb077363da6fef2d19b20a342e745a1dec43
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2022-01-09 15:49:55 +0900

    Gamne Over

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+// import express from 'express';
+
+import inquirer from 'inquirer';
+
+// const app = express();
+// const port = 3000;
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const chosenNumbers: number[] = [];
+const randomNumbers: number[] = [];
+
+const startApp = async (): Promise <void> => {
+  const validateInput = (text: string): boolean => {
+    const number = parseInt(text, 10);
+    return (number > 1 && number < 49 && !chosenNumbers.includes(number));
+  };
+  
+  do {
+    const result: any = await inquirer.prompt([{
+        name: 'number',
+        type: 'input',
+        message: 'Podaj liczbę...'
+    }]);
+
+    if (validateInput(result.number)) {
+        chosenNumbers.push(parseInt(result.number));
+    }
+    console.log(chosenNumbers);
+  } while (chosenNumbers.length < 6);
+  const randomNewNumber = (): number => {
+    return Math.floor(Math.random()* (49-1) + 1);
+  };
+
+  const validateRandomNumber = (number: number): boolean => {
+    return !randomNumbers.includes(number);
+  };
+
+  do {
+      const number: number = randomNewNumber();
+      if (validateRandomNumber(number)) {
+          randomNumbers.push(number);
+      }
+  } while (randomNumbers.length < 6);
+
+  let counter = 0;
+  chosenNumbers.forEach((number) => {
+    if(randomNumbers.includes(number)) {
+     counter += 1;
+    }
+  });
+  console.log(`Your score is ${counter}`);
+};
+startApp();
